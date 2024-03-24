5edf5527158b2408f440528bc82e5d332607cda7
John Doe <johndoe@Johns-MacBook-Air.local>
2024-03-24 13:39:12 +0300

    postgres connected

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang:1.22 as builder
 ENV GO111MODULE "on"
 WORKDIR /app
 COPY go.mod .
-# COPY go.sum .
+COPY go.sum .
 COPY . .
 RUN GOOS=linux GOARCH=amd64 go build -o main ./cmd/api-server/
 
--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
 
 func main() {
-	fmt.Println("hello world")
+	ctx := context.Background()
+
+	// TODO: make with ENVs
+	pgConn := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%d/%s?target_session_attrs=read-write",
+		"conf",
+		"pass",
+		"postgres",
+		5432,
+		"conf",
+	)
+
+	cfg, err := pgxpool.ParseConfig(pgConn)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.ConnConfig.PreferSimpleProtocol = true
+
+	pool, err := pgxpool.ConnectConfig(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(pool)
+	fmt.Println("Hello, world!")
 }
